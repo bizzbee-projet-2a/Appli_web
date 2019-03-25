ed0782e261760d0a8e36ee3a5877f0d767401a6f
thomasaudo <37633947+thomasaudo@users.noreply.github.com>
2019-03-25 14:00:41 +0100

    avec les bons idd

--- services/Api.js
+++ services/Api.js
@@ -7,10 +7,10 @@ var passwordHash = require('password-hash')
 var arrayToTree = require('array-to-tree');
 // Configuration pgsql
 const Bizbee = new pg.Pool({
-  user: 'postgres',
+  user: 'bizzbee',
   host: 'localhost',
-  database: 'postgres',
-  password: '',
+  database: 'bizzbee',
+  password: 'bizzbee',
   port: 5432
 });
 
